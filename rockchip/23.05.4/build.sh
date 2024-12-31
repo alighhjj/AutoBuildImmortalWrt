8156e3cfa6ed1b3513b37aa878ea56b8690d6f60
alighhjj <45477002+alighhjj@users.noreply.github.com>
2024-12-31 23:53:03 +0800

    恢复原定义

--- rockchip/23.05.4/build.sh
+++ rockchip/23.05.4/build.sh
@@ -25,9 +25,6 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
-PACKAGES="$PACKAGES npc"
-PACKAGES="$PACKAGES luci-app-npc"
-PACKAGES="$PACKAGES luci-i18n-npc-zh-cn"
 
 
 # 构建镜像
